526c5cb6ca1e3436e49392047dfc84399e8d9698
EuChawin <126758496+EuChawin@users.noreply.github.com>
2023-03-04 17:18:26 +0700

    Add files via upload

--- EP2/GUI-Knowledge.py
+++ EP2/GUI-Knowledge.py
@@ -4,22 +4,26 @@ from tkinter import messagebox
 
 GUI = Tk()  
 GUI.title('โปรแกรมบันทึกข้อมูล') #program title
-GUI.geometry('500x500') #screen size
-
+GUI.geometry('500x400') #screen size
 
 L1 = Label(GUI, text='โปรแกรมบันทึกความรู้', font=('Angsana New', 20), fg='blue')
-L1.place(x=20, y=100)
-b1 = ttk.Button(GUI,text='เงินมีอยู่กี่บาท?')
-b1.pack(ipadx=20, ipady=20) #expand the size of the button
-
-def Button2() : #button function
+L1.place(x=170, y=50)
+##################################################
+def Button1() : #button function
     text = 'ตอนนี้มีเงินในบัญชี 5 บาท'
-    messagebox.showinfo('เงินในบัญชี', text)
-
+    messagebox.showinfo('เงินในบัญชี',text)
 fb1 = Frame(GUI)
-fb1.place(x=100, y=200)
-b2 = ttk.Button(fb1, text='เงินมีอยู่กี่บาท ?', command=Button2)
+fb1.place(x=200, y=150)
+b1 = ttk.Button(fb1, text='เงินมีอยู่กี่บาท?', command=Button1)
+b1.pack(ipadx=20, ipady=20)
+##################################################
+def Button2() : #button function
+    text = 'เรียน Python 101'
+    messagebox.showinfo('วิชาที่เรียน',text)
+fb2 = Frame(GUI)
+fb2.place(x=200, y=250)
+b2 = ttk.Button(fb2, text='เรียนวิชาอะไร?', command=Button2)
 b2.pack(ipadx=20, ipady=20)
-
+##################################################
 
 GUI.mainloop()
